2af1db96051a742b3ae7c57d514d6cdd6ca395aa
jaymalave <jaym.github@gmail.com>
2021-09-07 00:48:28 +0530

    added linked list

---
+++ linkedList.cpp
@@ -0,0 +1,99 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class node{
+    public: 
+    int data;
+    node* next;
+    node(int val){
+        data = val;
+        next = NULL;
+    }
+};
+
+//inserting an element at the end
+void insertAtTail(node* &head, int val){
+   node* n = new node(val);
+
+   if(head == NULL){
+       head = n;
+       return;
+   }
+   node* temp = head;
+   while(temp->next != NULL){
+       temp = temp->next;
+   }
+   temp->next = n;
+}
+
+//displaying the linked list
+void display(node* &head){
+    node* temp = head;
+    while(temp != NULL){
+        cout<<temp->data<<"->";
+        temp = temp->next;
+    }
+    cout<<"NULL"<<endl;
+}
+
+//inserting at head
+void insertAtHead(node* &head, int val){
+    node* n = new node(val);
+    n->next = head;
+    head = n;
+}
+
+//search an element
+bool search(node* &head, int key){
+   node* temp = head;
+   while(temp != NULL){
+       if (temp->data == key){
+           return true;
+       }
+       temp=temp->next;
+   }
+   return false;
+}
+
+//deleting an element at head
+void deleteAtHead(node* &head){
+   node* toDelete = head;
+   head = head->next;
+   delete toDelete;
+}
+
+//deleting an element at any position
+void deletion(node* &head, int val){
+    if(head == NULL){
+        return;
+    }
+    if(head->next == NULL){
+        deleteAtHead(head);
+        return;
+    }
+
+    node* temp=head;
+    while(temp->next->data != val){
+        temp=temp->next;
+    }
+    node* toDelete = temp->next;
+    temp->next = temp->next->next;
+    delete toDelete;
+}
+
+int main(){
+    cout<<"Linked List Program"<<endl;
+    node* head = NULL;
+    insertAtTail(head, 1);
+    insertAtTail(head, 2);
+    insertAtTail(head, 3);
+    insertAtHead(head, 4);
+    deletion(head, 3);
+    display(head);
+    if (search(head, 3) == true){
+        cout<<'1'<<endl;
+    }else {
+        cout<<'0'<<endl;
+    }
+    return 0;
+}
