3105b3674942953ddd61ccd55c219766e606eb67
jaymalave <jaym.github@gmail.com>
2021-09-07 16:22:57 +0530

    reversing k nodes

--- linkedList.cpp
+++ linkedList.cpp
@@ -110,16 +110,38 @@ node* reverseRecur(node* &head){
    return newhead;
 }
 
+//revering k nodes
+node* reversek(node* &head, int k){
+    node* prevptr = NULL;
+    node* currptr = head;
+    node* nextptr;
+    int count = 0;
+    while(currptr != NULL && count<k){
+        nextptr = currptr->next;
+        currptr->next = prevptr;
+        prevptr = currptr;
+        currptr = nextptr;
+        count++;
+    }
+    if(nextptr != NULL){
+        head->next = reversek(nextptr, k);
+    }
+    return prevptr;
+}
+
 int main(){
     cout<<"Linked List Program"<<endl;
     node* head = NULL;
     insertAtTail(head, 1);
     insertAtTail(head, 2);
     insertAtTail(head, 3);
-    insertAtHead(head, 4);
-    deletion(head, 3);
+    insertAtTail(head, 4);
+    insertAtTail(head, 5);
+    insertAtTail(head, 6);
+    // deletion(head, 3);
     // reverseIter(head);
     //reverseRecur(head);
+    reversek(head, 2);
     display(head);
     if (search(head, 3) == true){
         cout<<'1'<<endl;
